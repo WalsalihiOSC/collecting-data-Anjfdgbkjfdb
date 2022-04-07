7fd2073ef36c51b2ee818ffbcb48f93087c0a3ee
bobo <79424554+Anjfdgbkjfdb@users.noreply.github.com>
2022-04-08 11:35:40 +1200

    ask users name,age and phone and appends details

--- CollectingData.py
+++ CollectingData.py
@@ -1,1 +1,27 @@
 #collecting data
+from tkinter import *
+
+class gui:
+    def __init__(self, parent): 
+        Label(parent,text="name").grid()
+        gui.name = Entry (parent,width=20)
+        gui.name.grid()
+        Label(parent,text="age").grid()
+        gui.age = Entry (parent,width=20)
+        gui.age.grid()  
+        self.select=StringVar()
+        self.select.set(0)
+        self.ans = ['Yes','No']
+        for item in self.ans:
+            gui.input=Radiobutton(text=item,value=item,variable = self.select)
+            gui.input.grid()
+        enter=Button(parent,text="save",command=self.save)
+        enter.grid()
+        self.info = []
+
+    def save(self):
+        self.info.append([gui.name,gui.age,gui.input])
+
+root = Tk()
+gui(root)
+root.mainloop()
