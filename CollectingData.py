08a5985f68fc63cd49e83986ca686b9f2fe42ff4
bobo <79424554+Anjfdgbkjfdb@users.noreply.github.com>
2022-04-12 14:15:20 +1200

    next and prev buttons added

--- CollectingData.py
+++ CollectingData.py
@@ -13,30 +13,47 @@ class gui:
         gui.age = Entry (self.f1,width=20)
         gui.age.grid()  
 
-        self.select=StringVar()
-        self.select.set(0)
+        gui.select=StringVar()
+        gui.select.set(0)
         self.ans = ['Yes','No']
         for item in self.ans:
-            gui.input=Radiobutton(self.f1,text=item,value=item,variable = self.select)
+            gui.input=Radiobutton(self.f1,text=item,value=item,variable = gui.select)
             gui.input.grid()
 
         enter=Button(self.f1,text="save",command=self.save)
         enter.grid()
         Button(self.f1,text="show", command=self.show).grid()
-        self.info = []
+        gui.info = [""""""]
 
-        
+        #==================================================================================================================================================
+        #==================================================================================================================================================
         self.f2=Frame(parent)
         self.f2.grid()
         self.f2.grid_forget()
-        Label(self.f2,text="name").grid()
+        self.i=0
         Button(self.f2,text="Enter data", command=self.enter_data).grid()
+        Button(self.f2,text="next", command=self.next).grid()
+        Button(self.f2,text="prev", command=self.prev).grid()
+
+
+    def next(self):
+        #print(gui.info[0])
+        self.i=self.i + 1
+        Label(self.f2, text=gui.info[self.i]).grid()
+
+    
+    def prev(self):
+        #print(gui.info[0])
+        self.i=self.i - 1
+        Label(self.f2, text=gui.info[self.i]).grid()
+
+        #==================================================================================================================================================
+        #==================================================================================================================================================
 
 
     def save(self):
-        self.info.append([gui.name,gui.age,gui.input])
-        Label(self.f1,text=self.info).grid()
-
+        gui.info.append([gui.name.get(), gui.age.get()  ,gui.select.get() ])        
+        
     def show(self):
         self.f1.grid_forget()
         self.f2.grid()
